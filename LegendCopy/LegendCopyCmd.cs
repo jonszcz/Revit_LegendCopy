b68b30bf86a0c29e4456d755af4f83ce879664f8
tlogan <tlogan@PWDA010>
2016-06-22 15:05:57 -0500

    Add project files.

---
+++ LegendCopy/LegendCopyCmd.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Windows.Media.Imaging;
+
+namespace Elk
+{
+    [Transaction(TransactionMode.Manual)]
+    public class LegendCopyCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+
+                // Get the current selection and make sure there's only one item selected.
+                Selection sel = uiDoc.Selection;
+                if (sel.GetElementIds().Count == 1)
+                {
+                    // Get the selected element
+                    Element e = uiDoc.Document.GetElement(sel.GetElementIds().First());
+
+                    // Make sure the element was retrieved and that it's a viewport element
+                    if (e != null && e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Viewports)
+                    {
+                        // Get the viewport
+                        Viewport vp = e as Viewport;
+
+                        // Get the legend view's name
+                        string viewName = vp.get_Parameter(BuiltInParameter.VIEWPORT_VIEW_NAME).AsString();
+
+                        // Get the viewport type
+                        ElementId vpTypeId = vp.get_Parameter(BuiltInParameter.ELEM_TYPE_PARAM).AsElementId();
+                        View view = uiDoc.Document.GetElement(vp.ViewId) as View;
+
+                        // Make sure the view is a legend view
+                        if (view.ViewType == ViewType.Legend)
+                        {
+                            // Get the location of the viewport
+                            XYZ loc = vp.GetBoxCenter();
+
+                            FilteredElementCollector sheetCollector = new FilteredElementCollector(uiDoc.Document);
+                            sheetCollector.OfClass(typeof(ViewSheet));
+                            List<ViewSheet> sheets = new List<ViewSheet>();
+                            foreach (ViewSheet vs in sheetCollector)
+                            {
+                                sheets.Add(vs);
+                            }
+
+                            // Pass the location, view, and viewport type to the form
+                            System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess();
+                            IntPtr handle = proc.MainWindowHandle;
+
+                            LegendCopyForm form = new LegendCopyForm(uiDoc.Document, view, vpTypeId, loc, sheets);
+                            System.Windows.Interop.WindowInteropHelper wih = new System.Windows.Interop.WindowInteropHelper(form);
+                            wih.Owner = handle;
+                            form.ShowDialog();
+                        }
+                        else
+                            TaskDialog.Show("Warning - 2", "Make sure only one Legend Viewport is selected before running this command.");
+                    }
+                    else
+                        TaskDialog.Show("Warning - 1", "Make sure only one Legend Viewport is selected before running this command.");
+                }
+                else
+                    TaskDialog.Show("Warning - 0", "Make sure only one Legend Viewport is selected before running this command.");
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+
+    public class LegendCopyApp : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string path = typeof(LegendCopyApp).Assembly.Location;
+
+            // Create the PushButtonData
+            PushButtonData legendCopyPBD = new PushButtonData(
+                "Legend Copy", "Legend\nCopy", path, "Elk.LegendCopyCmd")
+            {
+                LargeImage = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(Properties.Resources.Legend_32x32.GetHbitmap(), IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+                ToolTip = "Copy a legend view to multiple sheets in the same location.",
+            };
+
+            // Add the button to the ribbon
+            RevitCommon.UI.AddToRibbon(application, Properties.Settings.Default.TabName, Properties.Settings.Default.PanelName, legendCopyPBD);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ LegendCopy/LegendCopyForm.xaml.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using Autodesk.Revit.DB;
+
+namespace Elk
+{
+    /// <summary>
+    /// Interaction logic for LegendCopyForm.xaml
+    /// </summary>
+    public partial class LegendCopyForm : Window
+    {
+        LinearGradientBrush eBrush = null;
+        SolidColorBrush lBrush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0, 0, 0, 0));
+
+        // Revit Info
+        Document _doc;
+        XYZ _loc;
+        ElementId _vpType;
+        View _view;
+        List<ViewSheet> _sheets;
+
+        List<string> sheetParameterNames;
+        List<Parameter> sheetParameters;
+        List<ViewSheet> displaySheets;
+
+        Parameter filterParam = null;
+
+        public LegendCopyForm(Document doc, View view, ElementId vpType, XYZ loc, List<ViewSheet> sheets)
+        {
+            _doc = doc;
+            _loc = loc;
+            _vpType = vpType;
+            _view = view;
+            _sheets = sheets;
+
+            // fill the list of display sheets.  This will be the visible list rather than the full
+            // sheet set in case the filter is used.
+            displaySheets = sheets;
+            displaySheets.Sort((x, y) => x.SheetNumber.CompareTo(y.SheetNumber));
+
+            InitializeComponent();
+            GetSheetParameters();
+            GetDisplaySheets();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void closeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (eBrush == null)
+                eBrush = EnterBrush();
+
+            closeButtonRect.Fill = eBrush;
+        }
+
+        private void closeButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            closeButtonRect.Fill = lBrush;
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Transaction trans = new Transaction(_doc, "Legend Copy");
+                trans.Start();
+                foreach (object obj in sheetsListBox.SelectedItems)
+                {
+                    try
+                    {
+                        ViewSheet vs = obj as ViewSheet;
+                        Viewport vp = Viewport.Create(_doc, vs.Id, _view.Id, _loc);
+                        vp.get_Parameter(BuiltInParameter.ELEM_TYPE_PARAM).Set(_vpType);
+                    }
+                    catch { }
+                }
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                Autodesk.Revit.UI.TaskDialog.Show("Error", ex.Message);
+            }
+            Close();
+        }
+
+        private void okButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (eBrush == null)
+                eBrush = EnterBrush();
+
+            okButtonRect.Fill = eBrush;
+        }
+
+        private void okButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            okButtonRect.Fill = lBrush;
+        }
+
+        private LinearGradientBrush EnterBrush()
+        {
+            LinearGradientBrush b = new LinearGradientBrush();
+            b.StartPoint = new System.Windows.Point(0, 0);
+            b.EndPoint = new System.Windows.Point(0, 1);
+            b.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(255, 195, 195, 195), 0.0));
+            b.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(255, 245, 245, 245), 1.0));
+
+            return b;
+        }
+
+        private void GetSheetParameters()
+        {
+            sheetParameterNames = new List<string>();
+            sheetParameters = new List<Parameter>();
+
+            ViewSheet sheet = _sheets.First();
+            foreach (Parameter p in sheet.Parameters)
+            {
+                if (!sheetParameterNames.Contains(p.Definition.Name))
+                {
+                    sheetParameters.Add(p);
+                    sheetParameterNames.Add(p.Definition.Name);
+                }
+            }
+
+            sheetParameterNames.Sort();
+            sheetParameterNames.Insert(0, "(None)");
+            filterComboBox.ItemsSource = sheetParameterNames;
+            filterComboBox.SelectedIndex = 0;
+        }
+
+        private void filterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (filterComboBox.SelectedIndex != 0)
+            {
+                string paramName = filterComboBox.SelectedItem as string;
+                foreach (Parameter p in sheetParameters)
+                {
+                    if (paramName == p.Definition.Name)
+                    {
+                        filterParam = p;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                filterParam = null;
+            }
+
+            if (filterParam == null || (filterTextBox.Text != null || filterTextBox.Text.Length > 0))
+            {
+                GetDisplaySheets();
+            }
+        }
+
+        private void filterTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            GetDisplaySheets();
+        }
+
+        private void GetDisplaySheets()
+        {
+            displaySheets = new List<ViewSheet>();
+            if (filterComboBox.SelectedIndex > 0 && filterTextBox.Text != string.Empty)
+            {
+                List<ViewSheet> tempSheets = new List<ViewSheet>();
+                foreach (ViewSheet vs in _sheets)
+                {
+                    try
+                    {
+                        Parameter param = vs.get_Parameter(filterParam.Definition);
+                        if (null != param && (param.AsString().Contains(filterTextBox.Text) || param.AsValueString().Contains(filterTextBox.Text)))
+                        {
+                            tempSheets.Add(vs);
+                        }
+                    }
+                    catch { }
+                }
+
+                displaySheets = tempSheets;
+            }
+            else
+                displaySheets = _sheets;
+
+            displaySheets.Sort((x, y) => x.SheetNumber.CompareTo(y.SheetNumber));
+            sheetsListBox.ItemsSource = displaySheets;
+        }
+
+        private void Border_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control) // Is Ctrl key pressed
+            {
+                if (Keyboard.IsKeyDown(Key.U))
+                {
+                    // Launch the UI form
+                    System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess();
+                    IntPtr handle = proc.MainWindowHandle;
+                    RevitCommon.UILocation uiForm = new RevitCommon.UILocation("Legend Copy", Properties.Settings.Default.TabName, Properties.Settings.Default.PanelName);
+                    System.Windows.Interop.WindowInteropHelper wih = new System.Windows.Interop.WindowInteropHelper(uiForm);
+                    wih.Owner = handle;
+                    uiForm.ShowDialog();
+
+                    string tab = uiForm.Tab;
+                    string panel = uiForm.Panel;
+
+                    if (tab != Properties.Settings.Default.TabName || panel != Properties.Settings.Default.PanelName)
+                    {
+                        Properties.Settings.Default.TabName = tab;
+                        Properties.Settings.Default.PanelName = panel;
+                        Properties.Settings.Default.Save();
+
+                        Autodesk.Revit.UI.TaskDialog.Show("Warning", "Changes to the panel or tab this tool resides on will take place when Revit restarts.");
+                    }
+                }
+            }
+        }
+    }
+}
